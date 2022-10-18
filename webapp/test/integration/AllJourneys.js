e9d38c116c2690779311184be48b9bd453ad3d8e
KCsenge <csengekormoczy@gmail.com>
2022-10-18 16:13:34 +0200

    Testing Navigation

--- webapp/test/integration/AllJourneys.js
+++ webapp/test/integration/AllJourneys.js
@@ -1,13 +1,17 @@
-sap.ui.define([
-	"sap/ui/test/Opa5",
-	"./arrangements/Startup",
-	"./WorklistJourney"
-], function (Opa5, Startup) {
-	"use strict";
+sap.ui.define(
+  [
+    "sap/ui/test/Opa5",
+    "./arrangements/Startup",
+    "./WorklistJourney",
+    "./PostJourney",
+  ],
+  function (Opa5, Startup) {
+    "use strict";
 
-	Opa5.extendConfig({
-		arrangements: new Startup(),
-		viewNamespace: "sap.ui.demo.bulletinboard.view.",
-		autoWait: true
-	});
-});
+    Opa5.extendConfig({
+      arrangements: new Startup(),
+      viewNamespace: "sap.ui.demo.bulletinboard.view.",
+      autoWait: true,
+    });
+  }
+);
