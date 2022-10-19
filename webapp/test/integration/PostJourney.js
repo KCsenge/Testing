646179d0b2d735fc629bace4eb65aa7c0a4dd1cb
KCsenge <csengekormoczy@gmail.com>
2022-10-19 14:03:48 +0200

    Adding Tabs fixed

--- webapp/test/integration/PostJourney.js
+++ webapp/test/integration/PostJourney.js
@@ -42,16 +42,18 @@ sap.ui.define(
 
         // Assertions
         Then.onThePostPage.theTitleShouldDisplayTheName("Jeans");
-
-        // Cleanup
-        Then.iTeardownMyApp();
       }
     );
+
     opaTest("Should select the statistics tab", function (Given, When, Then) {
       // Actions
       When.onThePostPage.iPressOnTheTabWithTheKey("statistics");
+
       // Assertions
-      Then.onThePostPage.iShouldSeeTheViewCounter().and.iTeardownMyApp();
+      Then.onThePostPage.iShouldSeeTheViewCounter();
+
+      // Cleanup
+      Then.iTeardownMyApp();
     });
   }
 );
