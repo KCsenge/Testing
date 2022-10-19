524c5388e47070c36d2ba4a334d89aea7a123a38
KCsenge <csengekormoczy@gmail.com>
2022-10-19 16:26:15 +0200

    Adding the Date Formatter

--- webapp/test/unit/AllTests.js
+++ webapp/test/unit/AllTests.js
@@ -1,5 +1,10 @@
 sap.ui.define(
-  ["./model/models", "./model/formatter", "./model/FlaggedType"],
+  [
+    "./model/models",
+    "./model/formatter",
+    "./model/FlaggedType",
+    "./model/DateFormatter",
+  ],
   function () {
     "use strict";
   }
